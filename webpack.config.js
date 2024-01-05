95184d3c560c99df1524b072dea991811f6f987a
Bethq321 <bethq321@LaptopBeth>
2024-01-05 14:37:02 -0600

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 module.exports = {
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
+        test: /\.js$/,
         exclude: /node_modules/,
+        loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-react']
         }
       }
     ]
-  }
+  },
+  watchOptions: {
+    poll: true,
+  },
 };
